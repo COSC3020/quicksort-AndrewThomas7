d0971e55c44a9b24f27919e1595ece27c4748208
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-04-15 14:31:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,34 @@
-function quicksort(array) {
+function Swap(array, first, second) {
+    var temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
     return array;
+}
+
+function parititon(array,left,right) {
+    var j = 0;
+    var i = -1;
+    var pivot = array[right]
+    while (j < array.indexOf(pivot)) {
+        if (array[j] < pivot) {
+            i++
+            Swap(array, j, i)
+        }
+        j += 1;
+    }
+    i++
+    Swap(array, i, array.indexOf(pivot))
+    return i
+}
+
+function quicksort(array, left, right) {
+    if (array.length <= 1) {
+        return array;
+    }
+    var pivot = parititon(array, left, right);
+    if (left < right-1) {
+        quicksort(array, 0, pivot - 1)
+        quicksort(array, pivot + 1, right)
+    }
+    return array
 }
