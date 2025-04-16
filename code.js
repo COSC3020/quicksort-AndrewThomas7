2baf1aa55be33aeee1af140332c17c701e2a190b
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-04-15 20:00:01 -0600

    Update code.js

--- code.js
+++ code.js
@@ -5,8 +5,8 @@ function Swap(array, first, second) {
     return array;
 }
 
-function parititon(array,left,right) {
-    var j = 0;
+function parititon(array, left, right) {
+    var j = left;
     var i = -1;
     var pivot = array[right]
     while (j < array.indexOf(pivot)) {
@@ -16,20 +16,45 @@ function parititon(array,left,right) {
         }
         j += 1;
     }
-    i++
-    Swap(array, i, array.indexOf(pivot))
-    return i
+    // if (i != -1) {
+        i++
+        Swap(array, i, array.indexOf(pivot))
+        return i
+    // }
+    // return 0
 }
 
+function Sorted(array) {
+    sorted = false;
+    if (array.length == 1) {
+        return true
+    }
+    for (var x = 0; x < array.length - 1;) {
+        if (array[x] <= array[x + 1]) {
+            x++;
+            sorted = true;
+        }
+        else {
+            sorted = false;
+            break;
+        }
+    }
+    return sorted
+}
 
-function quicksort(array, left, right) {
-    if (array.length <= 1) {
-        return array;
+function quicksort(array) {
+    var left=0;
+    var right=array.length-1
+   function quicksort_(array, left, right){
+        if (array.length <= 1) {
+            return array;
+        }
+        var pivot = parititon(array, left,right);
+        console.log("Pivot: "+pivot)
+        if (Sorted(array)!=true) {
+            quicksort_(array, left, pivot - 1)
+        }
+        return array
     }
-    var pivot = parititon(array, left, right);
-    if (left < right-1) {
-        quicksort(array, 0, pivot - 1)
-        quicksort(array, pivot + 1, right)
-    }
-    return array
+    return quicksort_(array,left,right)
 }
