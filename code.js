86900e423ca9484c39733898516e638194199de0
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-04-16 11:21:25 -0600

    Update code.js

--- code.js
+++ code.js
@@ -5,33 +5,39 @@ function Swap(array, first, second) {
     return array;
 }
 
-function quicksort(array) {
-    function quicksort_(left,right){
-    if(array.length<=1){
-        return array
-    }
-    if(left< right){
-        var pivot=partition(left,right)
-        quicksort_(0,pivot-1)
-        quicksort_(pivot+1,right)
-    }
-
-    return array
-    }
-
-    function partition(left,right){
-        var i=left-1;
-        var pivot=array[right]
-        for(var x=left;x<=right;x++){
-            if(array[x]<=pivot){
-                i++
-                if(x>i){
-                    Swap(array,i,x)
+function quicksort(array){
+    var refereneArr=new Array(array.length).fill(0)
+        for(var m=0;m<refereneArr.length;m++){
+            var left=-5;
+            var right=-5;
+            for(var k=0;k<refereneArr.length;k++){
+               if(refereneArr[k]==0&& left==-5){
+                    left=k;
+               }
+               if(k==refereneArr.length-1&& refereneArr[k]!=1){
+                    right=k;
+                    break;
+               }
+               else if(refereneArr[k]==1&&left!=-5){
+                    right=k-1;
+                    break;
+               }
+            }
+            var pivot= partition(left,right)
+            refereneArr[pivot]=1;
+        }
+        function partition(left,right){
+            var i=left-1;
+            var pivot=array[right]
+            for(var x=left;x<=right;x++){
+                if(array[x]<=pivot){
+                    i++
+                    if(x>i){
+                        Swap(array,i,x)
+                    }
                 }
             }
+            return i;
         }
-        return i;
+        return array
     }
-
-    return quicksort_(0,array.length-1)
-}
